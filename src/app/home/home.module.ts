5edf8861c0fec792884c84fe30f65d338dc7790f
Niel Torres <ntorrema@nttdata.com>
2023-08-31 02:27:43 +0200

    first commit: Add page wedding invitation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { ModalComponent } from '../components/modal/modal.component';
 
 
 @NgModule({
@@ -14,6 +15,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, ModalComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, NgZone } from '@angular/core';
+import { Browser } from '@capacitor/browser';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+ 
+  isModalOpen = false;
 
   constructor() {}
+
+  loadData(evento: any){
+    console.log("Cargando");
+  }
+
+  reproducir() {
+    const audio = new Audio('./../../assets/music/a-thousand-years.mp3');
+    audio.play();
+  }
+
+  async gotoHashtag(){
+    let urlHashTag = "https://www.instagram.com/explore/tags/bodadegracedu";
+    await Browser.open({ url: urlHashTag });
+  }
+
+  async gotoWhatsapp(novia?: string){
+
+    let urlApiWhatsapp = "https://api.whatsapp.com/send?phone=";
+    let message = "&text=%C2%A1Hola%21+%F0%9F%8C%9F+Quiero+confirmar+mi+asistencia+a+la+boda+de+Edu+%26+Grace+%E2%99%A5%EF%B8%8F%F0%9F%92%8D%F0%9F%92%92";
+    let tfno_novio = "34649101596";
+    let tfno_novia = "34675575069";
+
+    let urlChatWhatsapp = urlApiWhatsapp+tfno_novio+message;
+    if(novia){
+      urlChatWhatsapp = urlApiWhatsapp+tfno_novia+message;
+    }
+    
+    await Browser.open({ url: urlChatWhatsapp });
+  }
+
+
+
 
 }
