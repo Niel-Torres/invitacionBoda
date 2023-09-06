765f54b041a7da2cd96eaaba87d253af2a9892d0
Niel Torres <ntorrema@nttdata.com>
2023-09-06 18:04:17 +0200

    fix error reload in home and new iamges added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,7 @@ import { App } from '@capacitor/app';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage {
 
   
   isModalOpen = false;
@@ -38,11 +38,11 @@ export class HomePage implements OnInit{
   }
 
   gotoWhatsapp(novia?: string) {
-
+    //https://api.whatsapp.com/send?phone=+34675575069&text=%C2%A1Hola%21+%F0%9F%8C%9F++Quiero+confirmar+mi+asistencia+a+tu+boda+%F0%9F%98%8D%E2%99%A5%EF%B8%8F%F0%9F%92%8D%F0%9F%92%92
     let urlApiWhatsapp = "https://api.whatsapp.com/send?phone=";
-    let message = "&text=%C2%A1Hola%21+%F0%9F%8C%9F+Quiero+confirmar+mi+asistencia+a+la+boda+de+Edu+%26+Grace+%E2%99%A5%EF%B8%8F%F0%9F%92%8D%F0%9F%92%92";
-    let tfno_novio = "34649101596";
-    let tfno_novia = "34675575069";
+    let message = "&text=%C2%A1Hola%21+%F0%9F%8C%9F++Quiero+confirmar+mi+asistencia+a+tu+boda+%F0%9F%98%8D%E2%99%A5%EF%B8%8F%F0%9F%92%8D%F0%9F%92%92";
+    let tfno_novio = "+34649101596";
+    let tfno_novia = "+34675575069";
 
     let urlChatWhatsapp = urlApiWhatsapp + tfno_novio + message;
     if (novia) {
